b2def2c7886bf03d4e899e35d1d54d28daf323aa
Nahirnegrooo <104226295+Nahirnegrooo@users.noreply.github.com>
2023-10-22 16:16:15 -0300

    Update Wendy.js

--- Wendy.js
+++ Wendy.js
@@ -1,18 +1,15 @@
+
 class Wendy {
- constructor (){
-  this.posX;
- this.posY;
- 
- }
-dibujar() {
- 
- }
-  movDerecha(){
-  
-  }
-  
-  movIzquierda(){
-  
+  constructor() {
+    this.posX = 5;
+    this.posY = 50;
+    this.tamX = 150;
+    this.tamY = 150;
+    this.img = loadImage("data/wendy.png");
   }
 
+  dibujar() {
+    image(this.img, this.posX, this.posY, this.tamX, this.tamY);
+  }
 }
+
