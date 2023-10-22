03439f1c141e2c23fd31495ea464038b0506a3ab
Nahirnegrooo <104226295+Nahirnegrooo@users.noreply.github.com>
2023-10-22 16:16:32 -0300

    Update tp5.js

--- tp5.js
+++ tp5.js
@@ -1,15 +1,18 @@
 let objJuego;
 
 function setup() {
-createCanvas(800,800);
+createCanvas(800,600);
 
 objJuego = new Juego();
 
 }
 
-
 function draw() {
-  
+ 
 objJuego.dibujar();
 
+}
+
+function keyPressed() {
+  objJuego.keyPressed();
 }
