20b30574ffdfcc3d11f7b5943ebd3dcc2e4c8700
Nahirnegrooo <104226295+Nahirnegrooo@users.noreply.github.com>
2023-10-22 16:15:35 -0300

    Update Bala.js

--- Bala.js
+++ Bala.js
@@ -1,22 +1,28 @@
 class Bala {
-
- constructor (){
-  this.posX= 100;
- this.posY = 100;
- 
- }
-dibujar() {
-  
-  fill(0);
-  ellipse(this.posX,this.posY);
-  
- }
-  movDerecha(){
-  
-  }
-  
-  movIzquierda(){
-  
+  constructor() {
+    this.posX = 0; // Debes asignarle las coordenadas iniciales
+    this.posY = 0;
+    this.disparada = false;
+    this.radio = 5; // Radio del círculo de la bala
   }
 
+  dibujar() {
+    if (this.disparada) {
+      push();
+      fill(0, 255, 0); // Color de la bala
+      ellipse(this.posX, this.posY, this.radio * 2);
+      pop();
+    }
+  }
+
+  mover() {
+    if (this.disparada) {
+      this.posY -= 5; // Cambia las coordenadas para mover la bala hacia arriba
+    }
+  }
+
+  disparar() {
+    this.disparada = true;
+  }
 }
+
