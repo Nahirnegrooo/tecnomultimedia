082180b918aa3016342ae783bf834d2f8105d04a
Nahirnegrooo <104226295+Nahirnegrooo@users.noreply.github.com>
2023-12-09 20:41:25 -0300

    Update Garfio.js

--- Garfio.js
+++ Garfio.js
@@ -1,23 +1,26 @@
-class Garfio {
-  constructor() {
+
+// pude lograr que la barra de vida funcione como corresponde, al disparar y la bala colisionar, el personaje se detiene de a poco
+// y la barra va disminuyendo hasta q al quedar sin vida pasa a la pantalla de ganar
+
+class Garfio {  //clase del villano del juego
+constructor() {
     this.posX = 660;
     this.posY = 50;
-    this.tamX = 150;
-    this.tamY = 150;
+    this.tamX = 170;
+    this.tamY = 170;
     this.img = loadImage("data/garfio.png");
     this.vida = 100;
-
-    this.velocidad = 1; // Velocidad de movimiento
-    
+    this.velocidad = -1; 
+    this.moverse=true;
+    this.parar=0;
   }
 
   dibujar() {
-    if (this.vida > 0) {
-      // Dibuja a Garfio
-      image(this.img, this.posX, this.posY, this.tamX, this.tamY);
+   
+      image(this.img, this.posX, this.posY, this.tamX, this.tamY);  // lo dibujo
 
-      // Dibuja la barra de vida
-      noFill();
+      
+      noFill();  // dibujo la barra de vida
       stroke(255);
       rect(this.posX, this.posY - 10, 30, 5);
 
@@ -25,40 +28,42 @@ class Garfio {
       let vidaActual = map(this.vida, 0, 100, 0, 30);
       rect(this.posX, this.posY - 10, vidaActual, 5);
     } 
-  }
 
-  mover() {
-    // Mueve a Garfio
-    this.posX += this.direccion * this.velocidad ;
 
+    mover() {  // moviento del villano, solo se mueve para una direccion 
    
+    if(this.moverse){
+    this.posX += this.velocidad;
+    }else{
+      if(frameCount%60){
+    this.parar++
+      }
+      if(this.parar%2){
+        this.moverse=true;
+      }
+    }
   }
 
-  colisionaConBala(bala) {
-    if (
-      bala.posX + bala.ancho >= this.posX &&
-      bala.posX <= this.posX + this.tamX &&
-      bala.posY + bala.alto >= this.posY &&
-      bala.posY <= this.posY + this.tamY
-    ) {
-     this.velocidad=0;
-     return true; // La bala colisionó con Garfio
+      colisionaConBala(bala) {
+    if ( bala.posX + bala.ancho >= this.posX && bala.posX <= this.posX + this.tamX && bala.posY + bala.alto >= this.posY && bala.posY <= this.posY + this.tamY  ) {
+     this.moverse=false;
+     return true; 
     }
     return false;
   }
-
+  
   colisionaConWendy(wendy) {
-    if (
-      this.posX + this.tamX >= wendy.posX &&
-      this.posX <= wendy.posX + wendy.tamX &&
-      this.posY + this.tamY >= wendy.posY &&
-      this.posY <= wendy.posY + wendy.tamY
-    ) {
-      return true; // Garfio colisionó con Wendy
+    if (this.posX + this.tamX >= wendy.posX && this.posX <= wendy.posX + wendy.tamX && this.posY + this.tamY >= wendy.posY && this.posY <= wendy.posY + wendy.tamY ) {
+      return true;
     }
     return false;
   }
 
+muerto(){   //garfio ya no tiene vidas
+  return this.vida<=0;
+  }
+  
+  
   recibirDaño(daño) {
     if (this.vida > 0) {
       this.vida -= daño;
