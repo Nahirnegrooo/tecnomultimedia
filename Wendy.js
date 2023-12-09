c7ff3723a94cfd5c0a0560f14456bc44f7341d24
Nahirnegrooo <104226295+Nahirnegrooo@users.noreply.github.com>
2023-12-09 20:44:30 -0300

    Update Wendy.js

--- Wendy.js
+++ Wendy.js
@@ -1,12 +1,11 @@
 
-class Wendy {
+class Wendy { // clase de la victima del juego
   constructor() {
     this.posX = 5;
     this.posY = 50;
-    this.tamX = 150;
-    this.tamY = 150;
+    this.tamX = 180;
+    this.tamY = 180;
     this.img = loadImage("data/wendy.png");
-    //this.velocidad= 1;
     this.vida= 2;
   }
 
@@ -14,14 +13,14 @@ class Wendy {
     image(this.img, this.posX, this.posY, this.tamX, this.tamY);
   }
 
- colisionaConBala(bala) {
+ colisionaConBala(bala) {  // logica para que al dispararle a wendy colisione con la bala
     if ( bala.posX + bala.ancho >= this.posX && bala.posX <= this.posX + this.tamX && bala.posY + bala.alto >= this.posY && bala.posY <= this.posY + this.tamY) {
      this.velocidad=0;
-     return true; // La bala colisionó con Wendy
+     return true; 
     }
     return false;
   }
-    recibirDaño(daño) {
+    recibirDaño(daño) {  // wendy recibe daño
     if (this.vida > 0) {
       this.vida -= daño;
       if (this.vida < 0) {
